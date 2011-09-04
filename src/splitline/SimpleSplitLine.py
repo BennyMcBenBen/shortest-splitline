2f03ea7530f1935bfb9da1e3d0c9f522eb7cd35f
Ben Zoller <benjamin.zoller@gmail.com>
2011-09-04 19:31:09 -0400

    working BFS

--- src/splitline/SimpleSplitLine.py
+++ src/splitline/SimpleSplitLine.py
@@ -4,6 +4,7 @@ Created on Aug 21, 2011
 @author: Ben
 '''
 import math
+from collections import deque
 
 class SimpleSplitLine(object):
     '''
@@ -16,14 +17,25 @@ class SimpleSplitLine(object):
         '''
         
     def printSplitLine(self, n):
-        if n == 1:
-            pass
-        else:
-            a = math.ceil(n/2)
-            b = math.floor(n/2)
-            print("a=" + str(a) + ", b=" + str(b))
-            self.printSplitLine(a)
-            self.printSplitLine(b)
+        out = []
+        
+        # BFS
+        working = deque()
+        working.append(n)
+        while len(working) > 0:
+            x = working.popleft()
+            out.append(x)
+            if x > 1:
+                a = math.ceil(x/2)
+                b = math.floor(x/2)
+                working.append(a)
+                working.append(b)
+        
+        print(out)
+        
+        # first level
+        if len(out) > 2:
+            print(str(out[0]) + " = " + str(out[1]) + " + " + str(out[2]))
         
 simple = SimpleSplitLine()
 simple.printSplitLine(7)
