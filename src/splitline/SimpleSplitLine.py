05fba0f49c8e0da5fbccc1c9304691ce112ca687
Ben Zoller <benjamin.zoller@gmail.com>
2011-09-04 19:27:55 -0400

    Revert "working BFS"
    This reverts commit 1118cb7daff794c89413596773b433ddbe773083.

--- src/splitline/SimpleSplitLine.py
+++ src/splitline/SimpleSplitLine.py
@@ -4,7 +4,6 @@ Created on Aug 21, 2011
 @author: Ben
 '''
 import math
-from collections import deque
 
 class SimpleSplitLine(object):
     '''
@@ -17,25 +16,14 @@ class SimpleSplitLine(object):
         '''
         
     def printSplitLine(self, n):
-        out = []
-        
-        # BFS
-        working = deque()
-        working.append(n)
-        while len(working) > 0:
-            x = working.popleft()
-            out.append(x)
-            if x > 1:
-                a = math.ceil(x/2)
-                b = math.floor(x/2)
-                working.append(a)
-                working.append(b)
-        
-        print(out)
-        
-        # first level
-        if len(out) > 2:
-            print(str(out[0]) + " = " + str(out[1]) + " + " + str(out[2]))
+        if n == 1:
+            pass
+        else:
+            a = math.ceil(n/2)
+            b = math.floor(n/2)
+            print("a=" + str(a) + ", b=" + str(b))
+            self.printSplitLine(a)
+            self.printSplitLine(b)
         
 simple = SimpleSplitLine()
 simple.printSplitLine(7)
